c850905189c4ced6a342470653e8afe7c61193dc
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2025-09-05 09:47:50 +0200

    2025/2026 review

--- src/lib_td1.cpp
+++ src/lib_td1.cpp
@@ -1,31 +1,37 @@
 #include "lib_td1.hpp"
 
 //----------------------------------------------------------------------------
-#ifdef  WITH_OVERLOAD
-void    DireBonjour(void){
+#ifdef WITH_OVERLOAD
+void DireBonjour(void)
+{
     std::cout << "Bonjour" << std::endl;
 }
 
-void    DireBonjour(std::string Nom){
+void DireBonjour(std::string Nom)
+{
     std::cout << "Old C++ : Bonjour " << Nom << std::endl;
-    std::println("C++23 : Bonjour {}",Nom);
+    std::println("C++23 : Bonjour {}", Nom);
 }
 #endif /*  WITH_OVERLOAD  */
 //----------------------------------------------------------------------------
-#ifdef  WITH_DEFAULT_VALUE
+#ifdef WITH_DEFAULT_VALUE
 
-void    DireBonjour(std::string Nom){
+void DireBonjour(std::string Nom)
+{
     std::cout << "Old C++ : Bonjour " << Nom << std::endl;
-    std::println("C++23 : Bonjour {}",Nom);
+    std::println("C++23 : Bonjour {}", Nom);
 }
 
-#endif  /*  WITH_DEFAULT_VALUE  */
+#endif /*  WITH_DEFAULT_VALUE  */
+
 //----------------------------------------------------------------------------
-double  CalculerSortieCapteur(double Entree, double Sensibilite, double Offset){
+double CalculerSortieCapteur(double Entree, double Sensibilite, double Offset)
+{
     return (Entree * Sensibilite) + Offset;
 }
 //----------------------------------------------------------------------------
-void    Swap(int* pa, int *pb){
+void Swap(int *pa, int *pb)
+{
     /*
     int tmp;
 
@@ -33,15 +39,11 @@ void    Swap(int* pa, int *pb){
     *pa = *pb;
     *pb = tmp;
     */
-   std::swap(*pa,*pb);
+    std::swap(*pa, *pb);
 }
 //----------------------------------------------------------------------------
-void    Swap(int &a, int &b){
-    std::swap(a,b);
+void Swap(int &a, int &b)
+{
+    std::swap(a, b);
 }
 //----------------------------------------------------------------------------
-
-
-
-
-
